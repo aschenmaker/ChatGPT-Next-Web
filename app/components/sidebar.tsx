3d32fc86edefb9c427d73c13f757fecd46ab6a3d
Qiu <aschen@cug.edu.cn>
2023-11-21 11:49:23 +0000

    fix sponsor

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          <b>Zhan ShaoWei</b> Special Sponsored Version.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
